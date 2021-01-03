a354612cd30916a8e9b8adeefd2b030a5008d47b
Leonid Levin <LevinLeonid2003@yandex.ru>
2021-01-03 10:32:12 +0300

    Solve problem with sending

--- main.py
+++ main.py
@@ -9,15 +9,19 @@ CHAT_ID_MINE = os.getenv('CHAT_ID_MINE')
 
 def main():
     bot = Bot(token=BOT_TOKEN)
-
     while True:
         try:
-            bot.send_message(chat_id=CHAT_ID, text='Moscow')
-            bot.send_message(chat_id=CHAT_ID_MINE, text='Moscow')
+            bot.send_message(chat_id=CHAT_ID_MINE, text='Have a break')
+            bot.send_message(chat_id=CHAT_ID, text="Wake up")
+
         except:
             print('Ошибка всего')
 
-        time.sleep(25 * 60)
+        time.sleep(20 * 60)
+        print("Hi")
+
+
+
 
 
 if __name__ == '__main__':
