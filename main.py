5c510f8c2c0e9b405e009950f52dab82577dc1af
Leonid Levin <LevinLeonid2003@yandex.ru>
2021-01-03 10:38:30 +0300

    Add minutes

--- main.py
+++ main.py
@@ -7,11 +7,18 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
 CHAT_ID_MINE = os.getenv('CHAT_ID_MINE')
 
+
 def main():
     bot = Bot(token=BOT_TOKEN)
+    count = 5
     while True:
         try:
-            bot.send_message(chat_id=CHAT_ID_MINE, text='Have a break')
+
+            if count >= 30:
+                count = 5
+
+            count += 1
+            bot.send_message(chat_id=CHAT_ID_MINE, text=f'Have a break for {count} minutes')
             bot.send_message(chat_id=CHAT_ID, text="Wake up")
 
         except:
